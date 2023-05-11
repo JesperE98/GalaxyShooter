15b0ce1c5959dd4a8c1eb3d72cdab45f80776a27
Jesper Elmtoft <113169402+JesperE98@users.noreply.github.com>
2023-05-12 00:20:58 +0200

    Beginning of project
    Added player (basic capsule) with a material and player script for basic movement of regular Vector3

---
+++ Space Shooter/Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime);
+    }
+}
