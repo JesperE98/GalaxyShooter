1a22994491dc2a48a4c72101eeddf1ddd83e462c
Jesper Elmtoft <113169402+JesperE98@users.noreply.github.com>
2023-07-31 01:26:26 +0200

    Revert "Fixed Compile errors"
    This reverts commit 1166af9e41814bf6879f67e1ac91b7dfbbf8ed1e.

--- Space Shooter/Assets/Galaxy Shooter/Scripts/SpawnManager.cs
+++ Space Shooter/Assets/Galaxy Shooter/Scripts/SpawnManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 
 public class SpawnManager : MonoBehaviour
